580e5cf69100f75c35fb6b7d32d219088ce29d77
laurengranada <laurengranada@utexas.edu>
2017-02-15 17:13:08 -0600

    adding updated files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_name) VALUES('Double Cheeseburger');
+INSERT INTO burgers (burgers_name) VALUES('Veggie Burger');
+INSERT INTO burgers (burgers_name) VALUES('Chicken Burger');
