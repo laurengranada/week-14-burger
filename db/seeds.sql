3b470e38571bf00f3e62c7acc2a9dd31f4d35f7d
laurengranada <laurengranada@utexas.edu>
2017-02-15 18:18:26 -0600

    added changed css

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burgers_name) VALUES('Double Cheeseburger');
-INSERT INTO burgers (burgers_name) VALUES('Veggie Burger');
-INSERT INTO burgers (burgers_name) VALUES('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES('Chicken Burger');
